ce05863dd6807deff45ba90c78b75da01788ae24
Stefan Folkesson <stefan.folkesson@ntig.se>
2022-06-27 19:10:59 +0200

    grunden är klar

--- todo.php
+++ todo.php
@@ -2,3 +2,4 @@
 // TODO: korrigera all output så den följer någon standard. Förmodligen json.
 // TODO: check valid date before insert
 // TODO: send varification or error after inserts
+// TODO: make editdata to a function
