de6382d455a68c8587d7556e2f5b77f5a7fc1d21
Stefan Folkesson <stefan.folkesson@ntig.se>
2022-06-27 12:00:15 +0200

    create done still need verification and error

--- createdata.php
+++ createdata.php
@@ -18,7 +18,98 @@ if(isset($_REQUEST['hash']) && isset($_REQUEST['anvnamn'])){
     // skapa period
     // skapa företag
     // skapa handledare
+        // Ny elev
+        if(isset($_REQUEST['pnr']) and 
+           isset($_REQUEST['fnamn']) and 
+           isset($_REQUEST['enamn']) and 
+           isset($_REQUEST['klass']) and
+           isset($_REQUEST['epost']) and 
+           isset($_REQUEST['nyelev'])
+           ){
+            $pnr=$_REQUEST['pnr'];
+            $fnamn=$_REQUEST['fnamn'];
+            $enamn=$_REQUEST['enamn'];
+            $klass=$_REQUEST['klass'];
+            $epost=$_REQUEST['epost'];
+            $sql="INSERT INTO elev (pnr,fnamn,enamn,klass,epost) VALUES (?,?,?,?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssss",$pnr,$fnamn,$enamn,$klass,$epost);
+            $stmt->execute();
+            // SEnd verification
 
+        }
+        // Ny period
+        elseif(isset($_REQUEST['periodnamn']) and 
+           isset($_REQUEST['start']) and 
+           isset($_REQUEST['nyperiod']) and  
+           isset($_REQUEST['slut'])  
+           ){
+            $periodnamn=$_REQUEST['periodnamn'];
+            $start=$_REQUEST['start'];
+            $slut=$_REQUEST['slut'];
+            $sql="INSERT INTO period (periodnamn,start,slut) VALUES (?,?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssss",$periodnamn,$start,$slut);
+            $stmt->execute();
+            // SEnd verification
+
+        } 
+        // Ny arbetsplats
+        elseif(isset($_REQUEST['foretagsnamn']) and 
+           isset($_REQUEST['kontaktnummer']) and 
+           isset($_REQUEST['eport']) and  
+           isset($_REQUEST['nyttforetag'])  
+           ){
+            $foretagsnamn=$_REQUEST['foretagsnamn'];
+            $kontaktnummer=$_REQUEST['kontaktnummer'];
+            $epost=$_REQUEST['epost'];
+            $sql="INSERT INTO arbetsplats (foretagsnamn,kontaktnummer,epost) VALUES (?,?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssss",$foretagsnamn,$kontaktnummer,$epost);
+            $stmt->execute();
+            // SEnd verification
+
+        }
+        // Ny handledare
+        elseif(isset($_REQUEST['anvandarnamn']) and 
+           isset($_REQUEST['losenord']) and 
+           isset($_REQUEST['fnamn']) and  
+           isset($_REQUEST['enamn']) and  
+           isset($_REQUEST['foretagid']) and  
+           isset($_REQUEST['nyhandledare'])  
+           ){
+            $anvandarnamn=$_REQUEST['anvandarnamn'];
+            $losenord=$_REQUEST['losenord'];
+            $fnamn=$_REQUEST['fnamn'];
+            $enamn=$_REQUEST['enamn'];
+            $foretagid=$_REQUEST['foretagid'];
+            $sql="INSERT INTO anvandare (admin,anvandarnamn,losenord,fnamn,enamn,foretagid) VALUES (0,?,?,?,?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssss",$anvandarnamn,$losenord,$fnamn,$enamn,$foretagid);
+            $stmt->execute();
+            // SEnd verification
+
+        }
+        // Ny placering
+        elseif(isset($_REQUEST['pnr']) and 
+           isset($_REQUEST['period']) and 
+           isset($_REQUEST['foretagnamn']) and  
+           isset($_REQUEST['nyplacering'])  
+           ){
+            $pnr=$_REQUEST['pnr'];
+            $period=$_REQUEST['period'];
+            $foretagnamn=$_REQUEST['foretagnamn'];
+            $sql="INSERT INTO placering (personnemmer,period,foretagsnamn) VALUES (?,?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sss",$pnr,$period,$foretagnamn);
+            $stmt->execute();
+            // SEnd verification
+
+        }
+
+        else {
+            echo "wrong indata";
+        }
     }
     else if(validhand($hash,$anv)){
 
--- todo.php
+++ todo.php
@@ -1,3 +1,4 @@
 // TODO: Klasser på elever  22/6
 // TODO: korrigera all output så den följer någon standard. Förmodligen json.
 // TODO: check valid date before insert
+// TODO: send varification or error after inserts
